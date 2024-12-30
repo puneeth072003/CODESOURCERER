09f79bc4298b1d1371bff8371b7d7bcaf2321628
Kushal Jetty <kushaljetty1203@gmail.com>
2024-12-30 22:25:17 +0530

    funtion to fetch yml contents added according to the requirement

--- src/github/controllers/initializers/fetchymlcontent.go
+++ src/github/controllers/initializers/fetchymlcontent.go
@@ -4,29 +4,46 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-// Config holds the YAML configuration fields
-type Config struct {
+// YMLConfig represents the overall structure of the YAML
+type YMLConfig struct {
+	Configuration ymlConfiguration `yaml:"configuration"`
+	Environment   ymlEnvironment   `yaml:"environment"`
+	Caching       ymlCaching       `yaml:"caching"`
+}
+
+// Configuration holds the YAML configuration fields
+type ymlConfiguration struct {
 	TestDirectory    string `yaml:"test-directory"`
 	Comments         string `yaml:"comments"`
 	TestingBranch    string `yaml:"testing-branch"`
 	TestingFramework string `yaml:"testing-framework"`
 	WaterMark        string `yaml:"water-mark"`
-	RedisCaching     string `yaml:"redis-caching"`
+}
+
+// Environment holds environment-specific configurations
+type ymlEnvironment struct {
+	PythonVersion string `yaml:"python-version"`
+}
+
+// Caching holds caching-related configurations
+type ymlCaching struct {
+	Enabled      bool   `yaml:"enabled"`
+	RedisCaching string `yaml:"redis-caching"`
 }
 
 // FetchAndReturnYAMLContents fetches YAML contents and returns it as a structure
-func FetchAndReturnYAMLContents(owner, repo, commitSHA, filePath string) (Config, error) {
+func FetchAndReturnYAMLContents(owner, repo, commitSHA, filePath string) (YMLConfig, error) {
 	// Fetch the file content from GitHub
 	content, err := FetchFileContentFromGitHub(owner, repo, commitSHA, filePath)
 	if err != nil {
-		return Config{}, err
+		return YMLConfig{}, err
 	}
 
 	// Parse YAML content
-	var config Config
+	var config YMLConfig
 	err = yaml.Unmarshal([]byte(content), &config)
 	if err != nil {
-		return Config{}, err
+		return YMLConfig{}, err
 	}
 
 	return config, nil
