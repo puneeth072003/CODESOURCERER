ac0005309293f1ef94d199bac35bea54fb5ce78b
Soorya U <sooryau7@gmail.com>
2024-12-30 19:19:06 +0530

    fix: Added URL Param Parsing for better security
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- src/github/controllers/initializers/fetchPRfiles.go
+++ src/github/controllers/initializers/fetchPRfiles.go
@@ -5,13 +5,38 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
+	"regexp"
+	"strconv"
 )
+
+func cleanURLParams(owner, repo string, prNumber int) (string, string, error) {
+	owner = url.QueryEscape(owner)
+	repo = url.QueryEscape(repo)
+
+	githubNameRegex := regexp.MustCompile(`^[a-zA-Z0-9-]+$`)
+
+	if !githubNameRegex.MatchString(owner) || !githubNameRegex.MatchString(repo) || prNumber <= 0 {
+		return "", "", fmt.Errorf("unable to clean url params")
+	}
+
+	return owner, repo, nil
+}
 
 // Fetch the list of changed files in the pull request
 func FetchPullRequestFiles(owner, repo string, prNumber int) ([]map[string]interface{}, error) {
-	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/pulls/%d/files", owner, repo, prNumber)
 
-	req, _ := http.NewRequest("GET", url, nil)
+	owner, repo, err := cleanURLParams(owner, repo, prNumber)
+	if err != nil {
+		return nil, err
+	}
+
+	reqUrl, err := url.JoinPath("https://api.github.com", "repos", owner, repo, "pulls", strconv.Itoa(123), "files")
+	if err != nil {
+		return nil, fmt.Errorf("unable to construct request url: %v", err)
+	}
+
+	req, _ := http.NewRequest("GET", reqUrl, nil)
 	req.Header.Set("Accept", "application/vnd.github.v3+json")
 
 	client := &http.Client{}
