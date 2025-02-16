2f156152d2704a4b7af838ecf5a15cb0dfa7d0b2
Soorya U <sooryau7@gmail.com>
2025-02-16 11:26:06 +0530

    feat: added retry client resolver
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- services/github/connections/genai.go
+++ services/github/connections/genai.go
@@ -26,3 +26,23 @@ func GetGeneratedTestsFromGenAI(payload *pb.GithubContextRequest) (*pb.Generated
 
 	return res, nil
 }
+
+func GetRetriedTestsFromGenAI(payload *pb.RetryMechanismPayload) (*pb.GeneratedTestsResponse, error) {
+	conn, err := getGrpcConnection(":9001")
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	client := pb.NewGenAiServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	res, err := client.GenerateRetriedTestFiles(ctx, payload)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
